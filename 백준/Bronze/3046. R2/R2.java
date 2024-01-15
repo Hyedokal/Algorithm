b83e1f34b0a7a6a233ed9a837cb9f772a8c7fb24
Hyedokal <137982252+Hyedokal@users.noreply.github.com>
2024-01-15 16:56:08 +0900

    [Bronze IV] Title: R2, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int avg = Integer.parseInt(s[1]);
+        int r1 = Integer.parseInt(s[0]);
+        System.out.println(avg*2 - r1);
+
+    }
+
+}
