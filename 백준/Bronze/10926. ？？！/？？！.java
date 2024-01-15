9efd023fef506633ebc295d4c1275db75f76803f
Hyedokal <137982252+Hyedokal@users.noreply.github.com>
2024-01-15 14:31:02 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int A;
+    static int B;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nickname = sc.nextLine();
+
+        System.out.println(nickname + "??!");
+    }
+
+}
