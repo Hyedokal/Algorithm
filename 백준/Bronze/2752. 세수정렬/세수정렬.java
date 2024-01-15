b126fe6e65ce1a38d837869994846da2bacb44df
Hyedokal <137982252+Hyedokal@users.noreply.github.com>
2024-01-15 16:50:51 +0900

    [Bronze IV] Title: 세수정렬, Time: 144 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int[] arr = new int[3];
+        for(int i=0; i<3; i++){
+            arr[i] = Integer.parseInt(s[i]);
+        }
+        Arrays.sort(arr);
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+
+    }
+
+}
