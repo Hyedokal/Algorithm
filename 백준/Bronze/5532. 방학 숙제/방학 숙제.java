829b6a73aba415b418248a573101075b839d5e4c
Hyedokal <137982252+Hyedokal@users.noreply.github.com>
2024-01-15 17:03:18 +0900

    [Bronze IV] Title: 방학 숙제, Time: 116 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        int math = (int) Math.ceil((double) B / D);
+        int korean = (int) Math.ceil((double) A / C);
+        System.out.println(L - Math.max(math, korean));
+    }
+
+}
