92fca0372e7380a2a50278a507f42ae840555680
Hyedokal <137982252+Hyedokal@users.noreply.github.com>
2024-03-27 17:25:33 +0900

    [Silver IV] Title: 로프, Time: 340 ms, Memory: 24808 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -6,17 +6,14 @@ import java.util.Comparator;
 
 public class Main {
     static int N;
-    static Integer[] ropes, prefix;
+    static Integer[] ropes;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         ropes = new Integer[N];
-        prefix = new Integer[N];
         for(int i=0; i<N; i++){
             ropes[i] = Integer.parseInt(br.readLine());
-            if(i==0) prefix[i] = ropes[i];
-            else prefix[i] = ropes[i]+ prefix[i-1];
         }
         Arrays.sort(ropes, new Comparator<Integer>() {
             @Override
@@ -24,7 +21,6 @@ public class Main {
                 return o2-o1;
             }
         });
-        Arrays.sort(prefix);
         int ans = 0;
         for(int i=0; i<N; i++){
             ans = Math.max(ans, (i+1)*ropes[i]);
